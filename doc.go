c16713241d7d9e35d0b89ae9a302f416a0c1c60b
Téo Bouvard <teobouvard@gmail.com>
2020-04-27 00:46:47 +0200

    Add module doc

---
+++ doc.go
@@ -0,0 +1,15 @@
+/*
+
+Package gotrace is a simple raytracer inspired by Peter Shirley's books.
+
+  package main
+
+  import "github.com/teobouvard/gotrace"
+
+  func main() {
+    scene := gotrace.BookScene()
+    scene.Render()
+  }
+
+*/
+package gotrace
